0b45e17feaea36ac9deec2f32f8c2d69da9048da
mohammedfaisal003 <146732816+mohammedfaisal003@users.noreply.github.com>
2023-12-27 15:12:32 +0000

    data

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// data types are catagarised based on how they are stored and aceesed in the memory
+// 1) primitive
+//   string , boolean , bigInt , number , null , undefined , symbol. 
+const score = 100
+const scoreValue = 100.36
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id===anotherId);
+
+
+// 2) reference(non primitive)
+//   Array , Objects, Functions.
+
+const heros = ["abubakar", "umar" , "osmaan" , "ali"]
+
+const details = {
+    name; "MFA"
+    
+}
+ 
